2f4e9b8ce8c3cc5b88d881c419599b4df6db942a
GloriousYellow <yellowglorious@gmail.com>
2017-10-12 19:51:39 -0400

    Initial commit 2

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # add-prettier
+Easily add prettier and eslint to an existing project!
+
+Run `npm i -g add-prettier` and then run `add-prettier` on a project to get started.
---
+++ index.js
@@ -0,0 +1,47 @@
+#!/usr/bin/env node
+
+const fs = require('fs-extra')
+const execa = require('execa')
+const ora = require('ora')
+
+async function addPrettier() {
+  const packageJSON = await fs.readJSON('package.json')
+
+  const spinner = ora('Adding prettier').start()
+  await execa('yarn', ['add', '-D', 'prettier-eslint-cli@1'])
+
+  spinner.start('Adding ESLint')
+  await execa('yarn', [
+    'add',
+    '-D',
+    'eslint',
+    'eslint-config-airbnb',
+    'eslint-plugin-jsx-a11y',
+    'eslint-plugin-import',
+    'eslint-plugin-react',
+  ])
+  spinner.succeed('Added prettier and ESLint')
+
+  packageJSON.prettier = {
+    singleQuote: true,
+    trailingComma: 'all',
+    semi: false,
+  }
+  packageJSON.eslintConfig = {
+    extends: 'airbnb',
+    rules: {
+      semi: ['error', 'never'],
+    },
+  }
+  packageJSON.scripts = packageJSON.scripts || {}
+  packageJSON.scripts.format =
+    "prettier-eslint --write '{src,test,app}/**/*.js'"
+  await fs.writeJSON('package.json', packageJSON, {
+    spaces: 2,
+  })
+  spinner.succeed('Wrote new config')
+}
+
+addPrettier().catch(e => {
+  throw e
+})
